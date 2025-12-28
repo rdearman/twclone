13d72f53b0881ebae6dfaaa3395ecf234e602d6d
rdearman <rick@rdearman.org>
2025-12-28 16:59:38 +0000

    tidy up

--- check_json.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-try:
-    with open('tests/tavern_tests.json', 'r') as f:
-        json.load(f)
-    print("JSON is valid")
-except json.JSONDecodeError as e:
-    print(f"JSON error: {e}")
